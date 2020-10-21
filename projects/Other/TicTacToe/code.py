0d90e1bfcfcd28bf20acecaa81e325e3ea13ac67
t-revor <48257612+t-revor@users.noreply.github.com>
2020-10-21 13:18:49 +0200

    Update code.py
    This is way better than the mess I made before..

--- projects/Other/TicTacToe/code.py
+++ projects/Other/TicTacToe/code.py
@@ -4,30 +4,15 @@
 # this should be intended to play with a numpad, for now
 
 # create board
-board = [['.','.','.'],
-          ['.','.','.'],
-          ['.','.','.']]
+BoardList = [' '] * 10
 
-def make_move():
-  # I want to use numpad keys as coordinates for X and O
-  
-  move = input("Input number from 1 to 9: ")
-  if move == str(1):
-    board[0][0] = 'X'
-  elif move == str(2):
-    board[0][1] = 'X'
-  elif move == str(3):
-    board[0][2] = 'X'
-  elif move == str(4):
-    board[1][0] = 'X'
-  elif move == str(5):
-    board[1][1] = 'X'
-  elif move == str(6):
-    board[1][2] = 'X'
-  elif move == str(7):
-    board[2][0] = 'X'
-  elif move == str(8):
-    board[2][1] = 'X'
-  elif move == str(9):
-    board[2][2] = 'X'
-  print(str(board[0]) + " \n" + str(board[1]) + " \n" + str(board[2]) + " \n")
+def draw_board(board):
+  print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + "\n")
+  print('------------')
+  print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + "\n")
+  print('------------')
+  print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + "\n")
+
+
+
+draw_board(BoardList)
